e6f50a6b07b3e115b6e32b17a82d1c986eb40f54
carlosbuenas870-sys <carlosbuenas870@gmail.com>
2025-10-30 15:38:48 -0500

    Delete xor_cipher.py

--- xor_cipher.py
+++
@@ -1,5 +0,0 @@
-import random
-key = ([str(random.randint(0, 1)) for i in range(50)])
-text = input("Texto: ")
-encrypted = ''.join([chr(ord(text[i]) ^ int(key[i % len(key)])) for i in range(len(text))])
-print("Llave:", key, "\nEncriptado:", encrypted, "\nDesencriptado:", ''.join([chr(ord(encrypted[i]) ^ int(key[i % len(key)])) for i in range(len(encrypted))]))
